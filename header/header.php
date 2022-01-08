04ddc003da52cf341f73abc39b2b3365c94a6459
Vova Chetvertinovskiy <mrkopir@gmail.com>
2022-01-08 17:13:32 +0200

    Css + Html

---
+++ header/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Online diary | Take bag and get this fu*king knowledge</title>
+</head>
+<body>
+    
+</body>
+</html>
