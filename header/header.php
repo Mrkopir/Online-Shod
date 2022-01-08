84f57095c3d98f9bf124f9daa6d5ec51f3549dae
Vova Chetvertinovskiy <mrkopir@gmail.com>
2022-01-08 17:50:58 +0200

    HTML PLUS

--- header/header.php
+++ header/header.php
@@ -2,10 +2,27 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Online diary | Take bag and get this fu*king knowledge</title>
+    <title>Online diary|Take bag and get this fu*king knowledge</title>
     <link rel='stylesheet' href="header/css/style.css">
 </head>
 <body>
-    
+    <header>
+        <div class="logo">
+            <img src="header/IMG/book_120884.png">
+            <h5>Online diary</h5>
+            <p>Take bag and get this fu*king knowledge</p>
+        </div>
+        <div class="nav">
+            <ul>
+                <li>Головна</li>
+                <li>Щоденник</li>
+                <li>Для батьків</li>
+                <li>Про нас</li>
+            </ul>
+        </div>
+        <div class="log">
+            <p><a href="#">SIGN IN</a> / <a href="#">LOG IN</a></p>
+        </div>
+    </header>
 </body>
 </html>
