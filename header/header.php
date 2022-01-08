61c679d7eda4f19e83a4da9976bb89f7453efe75
Vova Chetvertinovskiy <mrkopir@gmail.com>
2022-01-08 17:26:48 +0200

    scss

--- header/header.php
+++ header/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Online diary | Take bag and get this fu*king knowledge</title>
+    <link rel='stylesheet' href="header/css/style.css">
 </head>
 <body>
     
