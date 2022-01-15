684ee1005043669372baae655873762e395a572b
Vova Chetvertinovskiy <mrkopir@gmail.com>
2022-01-15 23:43:17 +0200

    Diary plus beta

--- diary/diary_main.php
+++ diary/diary_main.php
@@ -1,7 +1,13 @@
-    <table border="4" cols="4">
-    <span><tr>
-            <td class="rot_text" rowspan="7"><p class="rot_text_p">Понеділок</p></td>
-        </tr></span>
+<div class="diaryTitle">
+    <h2><time datetime="2022-01-15 19:00">15 Січня</time></h2>
+</div>
+
+<div class="AllTable">
+<div class="AllTable__Mon-Tus-Wen">
+<table border="1" cols="4">
+        <tr>
+            <td class="dataText" rowspan="7"><p class="dateText_rotateMonday elemant_rotate">Понеділок</p></td>
+        </tr>
         <tr>
             <td width="30px">1</td><td><input type="text" placeholder="Урок1" size="10"></td><td><input type="text" placeholder="ДЗ" size="25"></td><td>12</td>
         </tr>
@@ -19,6 +25,105 @@
         </tr>
         <tr>
             <td width="30px">6</td><td><input type="text" placeholder="Урок1" size="10"></td><td><input type="text" placeholder="ДЗ" size="25"></td><td>12</td>
-</tr>
+    </tr>
+</table>
 
-    </table>
+<table border="1" cols="4">
+        <tr>
+            <td class="dataText" rowspan="7"><p class="dateText_rotateTusday elemant_rotate">Вівторок</p></td>
+        </tr>
+        <tr>
+            <td width="30px">1</td><td><input type="text" placeholder="Урок1" size="10"></td><td><input type="text" placeholder="ДЗ" size="25"></td><td>12</td>
+        </tr>
+        <tr>
+            <td width="30px">2</td><td><input type="text" placeholder="Урок1" size="10"></td><td><input type="text" placeholder="ДЗ" size="25"></td><td>12</td>
+        </tr>
+        <tr>
+            <td width="30px">3</td><td><input type="text" placeholder="Урок1" size="10"></td><td><input type="text" placeholder="ДЗ" size="25"></td><td>12</td>
+        </tr>
+        <tr>
+            <td width="30px">4</td><td><input type="text" placeholder="Урок1" size="10"></td><td><input type="text" placeholder="ДЗ" size="25"></td><td>12</td>
+        </tr>
+        <tr>
+            <td width="30px">5</td><td><input type="text" placeholder="Урок1" size="10"></td><td><input type="text" placeholder="ДЗ" size="25"></td><td>12</td>
+        </tr>
+        <tr>
+            <td width="30px">6</td><td><input type="text" placeholder="Урок1" size="10"></td><td><input type="text" placeholder="ДЗ" size="25"></td><td>12</td>
+    </tr>
+</table>
+
+<table border="4" cols="4">
+        <tr>
+            <td class="dataText" rowspan="7"><p class="dateText_rotateWensday elemant_rotate">Середа</p></td>
+        </tr>
+        <tr>
+            <td width="30px">1</td><td><input type="text" placeholder="Урок1" size="10"></td><td><input type="text" placeholder="ДЗ" size="25"></td><td>12</td>
+        </tr>
+        <tr>
+            <td width="30px">2</td><td><input type="text" placeholder="Урок1" size="10"></td><td><input type="text" placeholder="ДЗ" size="25"></td><td>12</td>
+        </tr>
+        <tr>
+            <td width="30px">3</td><td><input type="text" placeholder="Урок1" size="10"></td><td><input type="text" placeholder="ДЗ" size="25"></td><td>12</td>
+        </tr>
+        <tr>
+            <td width="30px">4</td><td><input type="text" placeholder="Урок1" size="10"></td><td><input type="text" placeholder="ДЗ" size="25"></td><td>12</td>
+        </tr>
+        <tr>
+            <td width="30px">5</td><td><input type="text" placeholder="Урок1" size="10"></td><td><input type="text" placeholder="ДЗ" size="25"></td><td>12</td>
+        </tr>
+        <tr>
+            <td width="30px">6</td><td><input type="text" placeholder="Урок1" size="10"></td><td><input type="text" placeholder="ДЗ" size="25"></td><td>12</td>
+    </tr>
+</table>
+</div>
+<div class="AllTable__Tha-Fri-dop">
+<table border="4" cols="4">
+        <tr>
+            <td class="dataText" rowspan="7"><p class="dateText_rotateThatueday elemant_rotate">Четвер</p></td>
+        </tr>
+        <tr>
+            <td width="30px">1</td><td><input type="text" placeholder="Урок1" size="10"></td><td><input type="text" placeholder="ДЗ" size="25"></td><td>12</td>
+        </tr>
+        <tr>
+            <td width="30px">2</td><td><input type="text" placeholder="Урок1" size="10"></td><td><input type="text" placeholder="ДЗ" size="25"></td><td>12</td>
+        </tr>
+        <tr>
+            <td width="30px">3</td><td><input type="text" placeholder="Урок1" size="10"></td><td><input type="text" placeholder="ДЗ" size="25"></td><td>12</td>
+        </tr>
+        <tr>
+            <td width="30px">4</td><td><input type="text" placeholder="Урок1" size="10"></td><td><input type="text" placeholder="ДЗ" size="25"></td><td>12</td>
+        </tr>
+        <tr>
+            <td width="30px">5</td><td><input type="text" placeholder="Урок1" size="10"></td><td><input type="text" placeholder="ДЗ" size="25"></td><td>12</td>
+        </tr>
+        <tr>
+            <td width="30px">6</td><td><input type="text" placeholder="Урок1" size="10"></td><td><input type="text" placeholder="ДЗ" size="25"></td><td>12</td>
+    </tr>
+</table>
+
+<table border="4" cols="4">
+        <tr>
+            <td class="dataText" rowspan="7"><p class="dateText_rotateFriday elemant_rotate">П'ятниця</p></td>
+        </tr>
+        <tr>
+            <td width="30px">1</td><td><input type="text" placeholder="Урок1" size="10"></td><td><input type="text" placeholder="ДЗ" size="25"></td><td>12</td>
+        </tr>
+        <tr>
+            <td width="30px">2</td><td><input type="text" placeholder="Урок1" size="10"></td><td><input type="text" placeholder="ДЗ" size="25"></td><td>12</td>
+        </tr>
+        <tr>
+            <td width="30px">3</td><td><input type="text" placeholder="Урок1" size="10"></td><td><input type="text" placeholder="ДЗ" size="25"></td><td>12</td>
+        </tr>
+        <tr>
+            <td width="30px">4</td><td><input type="text" placeholder="Урок1" size="10"></td><td><input type="text" placeholder="ДЗ" size="25"></td><td>12</td>
+        </tr>
+        <tr>
+            <td width="30px">5</td><td><input type="text" placeholder="Урок1" size="10"></td><td><input type="text" placeholder="ДЗ" size="25"></td><td>12</td>
+        </tr>
+        <tr>
+            <td width="30px">6</td><td><input type="text" placeholder="Урок1" size="10"></td><td><input type="text" placeholder="ДЗ" size="25"></td><td>12</td>
+    </tr>
+</table>
+    <textarea name="comment" cols="40" rows="3" placeholder="Додатково"></textarea>
+</div>
+</div>
