fd975ee00f0555f4242b069e0d955cccf132a210
Vova Chetvertinovskiy <mrkopir@gmail.com>
2022-01-16 16:41:38 +0200

    About beta plus

--- about/about_main.php
+++ about/about_main.php
@@ -1,8 +1,36 @@
 <div class="about">
-<div class="TitleName">
-    <h1>Четвертинівський Вова — <br>WEB Developer — <br>Ukraine, Illintsi</h1>
+    <div class="First_column">
+        <div class="TitleName">
+            <h1>Четвертинівський Вова — <br>WEB Developer — <br>Ukraine, Illintsi</h1>
+        </div>
+        <div class="About_text">
+            <p>Привіт, мене звати Вова (14(2007р)),<br>Іллінецька ЗОШ I-IIIст Гімназія №2<br> я ВЕБ розробник! <br> Володію HTML5 CSS3 JS PYTHON.</p>
+        </div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d653.0155805739398!2d29.20182279225818!3d49.104456482846686!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d2bc813f941eff%3A0x50daa04c1c0b911b!2sSchool-Gymnasium%20%E2%84%962!5e0!3m2!1sru!2sua!4v1642342196478!5m2!1sru!2sua" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+    </div>
+    <div class="Secound_column">
+        <div class="Skills">
+            <h3>Навики</h3>
+            <p>HTML5<br>CSS3<br><u>JS</u><br>PYTHON<br><u>PHP</u></p>
+        </div>
+        <div class="Skills_dop">
+            <h3>Препроцесори<br>Фраймворки</h3>
+            <p>SASS<br>SCSS<br>BOOTSTRAP</p>
+        </div>
+    </div>
+    <div class="Thrit_column">
+        <div class="About_progect">
+            <h2>Про проект</h2>
+            <p>Проек розробляється для використання у навчальних цілях,<br>для зручного використання учнями,<br>адже в наш час багато школярів мають гаджети!<br>З огляду на виклики та нагальні потреби, які постали перед системою освіти України в умовах протидії пандемії COVID-19, та з метою дерегуляції та дебюрократизації управління системою освіти, спрощення ведення документообігу закладів загальної середньої освіти (далі – ЗЗСО) Міністерством освіти і науки України було поставлено завдання ДНУ «Інститут освітньої аналітики» (наказ МОН № 781 від 09.06.2020) забезпечити розробку державних безкоштовних електронних журналів та щоденників в рамках розвитку Програмно-апаратного комплексу «Автоматизований комплекс освітнього менеджменту» (далі – ПАК «АІКОМ»), який є модернізованою системою ІТС «ДІСО». Впровадження даного програмного продукту є інновацією, що надасть для педагогічних працівників, батьків та учнів ЗЗСО зручний онлайн-інструмент, який 24/7 забезпечить доступ до оцінок, відвідуваності та інших ресурсів журналу. Впровадження такого ІТ рішення є особливо актуальним для тих ЗЗСО та громад, які не можуть собі дозволити закупівлю альтернативних комерційних програмних продуктів.</p>       
+        </div>
+        <div class="CathPhare">
+            <p>Take bag and keep this fu*king knowledge</p>
+        </div>
+        <div class="soc">
+            <p><a href="https://www.instagram.com/vova_chetvertinovski/">Instagram</a></p>
+            <p><a href="https://github.com/Mrkopir">Github</a></p>
+            <p><a href="https://www.tiktok.com/@program_ist">TikTok</a></p>
+        </div>
+    </div>
 </div>
-<div class="About_text">
-    <p>Привіт, мене звати Вова,<br> я ВЕБ розробник! <br> Володію HTML5 CSS3 JS PYTHON.</p>
-</div>
-</div>
+
